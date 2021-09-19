b819d2615a26cbdca8eae80a27797555f7add9b4
ronah289 <ronald.kibet@student.moringaschool.com>
2021-09-19 01:24:57 -0230

    edit pizza processing function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-//initialize values
-//prices
 var crustPrice = new Array(250,200,350);
 var sizePrice = new Array(350,500,700);
 var toppingsPrice = new Array(140,180,220);
-class Customer {
-    constructor(flavor, crust, size, toppings) {
-        this.flavor = flavor;
-        this.crust = crust;
-        this.size = size;
-        this.toppings = toppings;
-    }
+toppingOptions = new Array("garlic-butter","pepperoni","proscuitto");
+// Business Logic
+function processPizza(flavor,size,crust,topping){
+    this.flavor=flavor;
+    this.size=size;
+    this.crust=crust;
+    this.topping=topping;
 }
-
+// user 
 $(document).ready(function(){
-$("form#order-form").submit(function(event){
-    //flavor
-    var selectedFlavor = document.getElementsByName("flavors");
-    for(i=0;i<selectedFlavor.length;i++){
-        if(selectedFlavor[i].checked){
-            // alert(selectedFlavor[i].value);
-            alert(i);
-        }
-    }
-});
+    $("form#order-form").submit(function(){
+        event.preventDefault();
+        let flavor = $('input[name="flavors"]:checked').val();
+        let crust = $('input[name="crust"]:checked').val();
+        let size = $('input[name="pizza-size"]:checked').val();
+        let toppingSelected = [];
+        $.each($("input[name='offers']:checked"), function() {
+            toppingSelected.push($(this).val());
+        });
+    });
 });
